82cf1b0ed016498fee9ebfd519c9ccf48a32bf38
Jonathan Erthal <jonierthal@gmail.com>
2023-05-16 08:10:48 -0300

    Componente: SearchForm

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -11,7 +11,7 @@ export const TransactionsTable = styled.table`
     width: 100%;
     border-collapse: separate;
     border-spacing: 0 0.5rem;
-    /*margin-top: 1.5rem */
+    margin-top: 1.5rem;
 
     td{
         padding: 1.25rem 2rem;
@@ -35,5 +35,9 @@ interface PriceHighlightProps {
 }
 
 export const PriceHighlight = styled.span<PriceHighlightProps>`
-    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]}
+    color: ${props => 
+                props.variant === 'income' 
+                ? props.theme["green-300"] 
+                : props.theme["red-300"]
+            }
 `;
