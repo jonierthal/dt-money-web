f8dd4aa0e3207a5936f365a3a778755bc3dd52da
Jonathan Erthal <jonierthal@gmail.com>
2023-06-01 08:18:06 -0300

    Corrigindo erros de linting

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,108 +1,112 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
-import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
-import * as z from 'zod';
-import { Controller, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { api } from '../../lib/axios';
-import { useContext } from 'react';
-import { TransactionsContext } from '../../contexts/TransactionsContext';
+import * as Dialog from '@radix-ui/react-dialog'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
+import * as z from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 const newTransactionFormSchema = z.object({
-    description: z.string(),
-    price: z.number(),
-    category: z.string(),
-    type: z.enum(['income', 'outcome']),
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
 })
 
-type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-    const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useContext(TransactionsContext)
 
-    const {
-        control,
-        register,
-        handleSubmit,
-        formState: { isSubmitting },
-        reset,
-    } = useForm<NewTransactionFormInputs>({
-        resolver: zodResolver(newTransactionFormSchema),
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    reset,
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    const { description, price, category, type } = data
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
     })
 
-    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-        const { description, price, category, type } = data;
+    reset()
+  }
 
-        await createTransaction({
-            description,
-            price,
-            category,
-            type,
-        })
+  return (
+    <Dialog.Portal>
+      <Overlay />
 
-        reset();
-    }
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
 
-    return (
-        <Dialog.Portal>
-            <Overlay />
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
 
-            <Content>
-                <Dialog.Title>Nova transação</Dialog.Title>
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Desrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
 
-                <CloseButton>
-                    <X size={24}/>
-                </CloseButton>
-                
-                <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
-                    <input 
-                        type="text" 
-                        placeholder="Desrição" 
-                        required
-                        {...register('description')} 
-                    />
-                    <input 
-                        type="number" 
-                        placeholder="Preço" 
-                        required
-                        {...register('price', {valueAsNumber: true})} 
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Categoria" 
-                        required
-                        {...register('category')} 
-                    />
-                    
-                    <Controller
-                        control={control}
-                        name="type"
-                        render={({ field }) => {
-                            return (
-                                <TransactionType 
-                                    onValueChange={field.onChange} 
-                                    value={field.value}
-                                >
-                                    <TransactionTypeButton variant='income' value='income'>
-                                        <ArrowCircleUp size={24}/>
-                                        Entrada
-                                    </TransactionTypeButton>
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
 
-                                    <TransactionTypeButton variant='outcome' value='outcome'>
-                                        <ArrowCircleDown size={24}/>
-                                        Saída
-                                    </TransactionTypeButton>
-                                </TransactionType>
-                            )
-                        }}
-                    />
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
 
-                    <button type="submit" disabled={isSubmitting}>
-                        Cadastrar
-                    </button>
-                </form>
-
-            </Content>
-        </Dialog.Portal>
-    )
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,39 +1,39 @@
-import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
-import { SummaryContainer, SummaryCard } from "./styles";
-import { priceFormatter } from "../../utils/formatter";
-import { useSumarry } from "../../hooks/useSummary";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+import { SummaryContainer, SummaryCard } from './styles'
+import { priceFormatter } from '../../utils/formatter'
+import { useSumarry } from '../../hooks/useSummary'
 
-export function Summary(){
-    const summary = useSumarry();
+export function Summary() {
+  const summary = useSumarry()
 
-    return(
-        <SummaryContainer >
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e"/>
-                </header>
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
 
-                <strong>{priceFormatter.format(summary.income)}</strong>
-            </SummaryCard>
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </SummaryCard>
 
-            <SummaryCard>
-                <header>
-                    <span>Saídas</span>
-                    <ArrowCircleDown size={32} color="#f75a68"/>
-                </header>
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
 
-                <strong>{priceFormatter.format(summary.outcome)}</strong>
-            </SummaryCard>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
+      </SummaryCard>
 
-            <SummaryCard variant="green">
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#fff"/>
-                </header>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
 
-                <strong>{priceFormatter.format(summary.total)}</strong>
-            </SummaryCard>
-        </SummaryContainer>
-    )
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
 }
